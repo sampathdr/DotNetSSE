f9c8d71efbe3788927f60d81e2cf1aa566a015b4
sampathdr <sampath.dr@gmail.com>
2026-03-26 23:15:59 +0100

    Adding web app

--- DotNetServerSendEvents.Api/Program.cs
+++ DotNetServerSendEvents.Api/Program.cs
@@ -1,6 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Logging.AddConsole();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowAnyOrigin();
+    });
+});
 
+
+var app = builder.Build();
+app.UseCors();
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/stream", async (HttpContext context) =>
+{
+    context.Response.Headers.Add("Content-Type", "text/event-stream");
+
+    while (!context.RequestAborted.IsCancellationRequested)
+    {
+        var data = $"data: {DateTime.Now}\n\n";
+        await context.Response.WriteAsync(data);
+        await context.Response.Body.FlushAsync();
+        await Task.Delay(2000);
+    }
+});
 
 app.Run();
